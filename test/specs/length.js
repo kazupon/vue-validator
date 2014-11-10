d71edac1135b88fe87bceb66e2257b6142cfabf2
kazuya kawaguchi <kawakazu80@gmail.com>
2014-11-11 01:08:28 +0900

    Add spec template

---
+++ test/specs/length.js
@@ -0,0 +1,26 @@
+/**
+ * import(s)
+ */
+
+var Vue = require('../../node_modules/vue/dist/vue')
+var plugin = require('../../index')
+
+
+describe('minLength', function () {
+  // TODO: 
+  
+  it('should be ok', function (done) {
+    expect(true).to.be(true)
+    done()
+  })
+})
+
+
+describe('maxLength', function () {
+  // TODO: 
+  
+  it('should be ok', function (done) {
+    expect(true).to.be(true)
+    done()
+  })
+})
---
+++ test/specs/numeric.js
@@ -0,0 +1,26 @@
+/**
+ * import(s)
+ */
+
+var Vue = require('../../node_modules/vue/dist/vue')
+var plugin = require('../../index')
+
+
+describe('min', function () {
+  // TODO: 
+  
+  it('should be ok', function (done) {
+    expect(true).to.be(true)
+    done()
+  })
+})
+
+
+describe('max', function () {
+  // TODO: 
+  
+  it('should be ok', function (done) {
+    expect(true).to.be(true)
+    done()
+  })
+})
