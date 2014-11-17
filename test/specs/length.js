7eb3137f446fa81dcab86153a0ad939c7539c58d
kazuya kawaguchi <kawakazu80@gmail.com>
2014-11-18 03:11:56 +0900

    Implement minLength/maxLength validator spec

--- test/specs/length.js
+++ test/specs/length.js
@@ -3,24 +3,122 @@
  */
 
 var Vue = require('../../node_modules/vue/dist/vue')
-var plugin = require('../../index')
+var validator = require('../../index')
+var createInstance = require('./helper').createInstance
 
 
 describe('minLength', function () {
-  // TODO: 
+  var vm, target
+
+  before(function () {
+    Vue.config.async = false
+    Vue.use(validator)
+  })
+
+  after(function () {
+    Vue.config.async = true
+  })
   
-  it('should be ok', function (done) {
-    expect(true).to.be(true)
-    done()
+  beforeEach(function () {
+    vm = createInstance(
+      '<input type="text" v-model="comment" v-validate="minLength: 4">',
+      { msg: null }
+    )
+    target = vm._children[0]
+  })
+
+
+  describe('when input 3 length string', function () {
+    beforeEach(function () {
+      vm.comment = 'aaa'
+      vm._digest() // force update
+    })
+
+    it('should be true', function () {
+      expect(target.validation.comment.minLength).to.be(true)
+    })
+  })
+
+
+  describe('when input 4 length string', function () {
+    beforeEach(function () {
+      vm.comment = 'aaaa'
+      vm._digest() // force update
+    })
+
+    it('should be false', function () {
+      expect(target.validation.comment.minLength).to.be(false)
+    })
+  })
+
+
+  describe('when input 5 length string', function () {
+    beforeEach(function () {
+      vm.comment = 'aaaaa'
+      vm._digest() // force update
+    })
+
+    it('should be false', function () {
+      expect(target.validation.comment.minLength).to.be(false)
+    })
   })
 })
 
 
+
 describe('maxLength', function () {
-  // TODO: 
+  var vm, target
+
+  before(function () {
+    Vue.config.async = false
+    Vue.use(validator)
+  })
+
+  after(function () {
+    Vue.config.async = true
+  })
   
-  it('should be ok', function (done) {
-    expect(true).to.be(true)
-    done()
+  beforeEach(function () {
+    vm = createInstance(
+      '<input type="text" v-model="comment" v-validate="maxLength: 4">',
+      { msg: null }
+    )
+    target = vm._children[0]
+  })
+
+
+  describe('when input 3 length string', function () {
+    beforeEach(function () {
+      vm.comment = 'aaa'
+      vm._digest() // force update
+    })
+
+    it('should be false', function () {
+      expect(target.validation.comment.maxLength).to.be(false)
+    })
+  })
+
+
+  describe('when input 4 length string', function () {
+    beforeEach(function () {
+      vm.comment = 'aaaa'
+      vm._digest() // force update
+    })
+
+    it('should be false', function () {
+      expect(target.validation.comment.maxLength).to.be(false)
+    })
+  })
+
+
+  describe('when input 5 length string', function () {
+    beforeEach(function () {
+      vm.comment = 'aaaaa'
+      vm._digest() // force update
+    })
+
+    it('should be true', function () {
+      expect(target.validation.comment.maxLength).to.be(true)
+    })
   })
 })
