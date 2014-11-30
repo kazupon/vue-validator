62ea6592fb8586f843af18f4979c6775e652f6d4
kazuya kawaguchi <kawakazu80@gmail.com>
2014-12-01 03:03:11 +0900

    Add valid and invalid specs

---
+++ test/specs/valid.js
@@ -0,0 +1,105 @@
+/**
+ * Import(s)
+ */
+
+var Vue = require('../../node_modules/vue/dist/vue')
+var validator = require('../../index')
+var createInstance = require('./helper').createInstance
+
+
+describe('valid', function () {
+  var vm, targetVM
+
+  before(function () {
+    Vue.config.async = false
+  })
+
+  after(function () {
+    Vue.config.async = true
+  })
+
+  beforeEach(function () {
+    var inputs = '<input type="text" v-model="username" v-validate="required, minLength: 4, maxLength: 16">'
+      + '<input type="text" v-model="zip" v-validate="required, pattern: /^[0-9]{3}-[0-9]{4}$/"'
+    vm = createInstance({
+       target: inputs,
+       validator: validator,
+       data: function () { return { username: '', zip: '' } }
+    })
+    targetVM = vm._children[0]
+  })
+
+  
+  describe('init instance', function () {
+    describe('validation.username.valid', function () {
+      it('should be false', function () {
+        expect(targetVM.validation.username.valid).to.be(false)
+      })
+    })
+
+    describe('validation.zip.valid', function () {
+      it('should be false', function () {
+        expect(targetVM.validation.zip.valid).to.be(false)
+      })
+    })
+
+    describe('valid', function () {
+      it('should be false', function () {
+        expect(targetVM.valid).to.be(false)
+      })
+    })
+  })
+
+
+  describe('pass username validation', function () {
+    beforeEach(function () {
+      vm.username = 'kazupon'
+      vm._digest() // force update
+    })
+
+    describe('validation.username.valid', function () {
+      it('should be true', function () {
+        expect(targetVM.validation.username.valid).to.be(true)
+      })
+    })
+
+    describe('validation.zip.valid', function () {
+      it('should be false', function () {
+        expect(targetVM.validation.zip.valid).to.be(false)
+      })
+    })
+
+    describe('valid', function () {
+      it('should be false', function () {
+        expect(targetVM.valid).to.be(false)
+      })
+    })
+  })
+
+
+  describe('pass username and zip validation', function () {
+    beforeEach(function () {
+      vm.username = 'kazupon'
+      vm.zip = '111-2222'
+      vm._digest() // force update
+    })
+
+    describe('validation.username.valid', function () {
+      it('should be true', function () {
+        expect(targetVM.validation.username.valid).to.be(true)
+      })
+    })
+
+    describe('validation.zip.valid', function () {
+      it('should be true', function () {
+        expect(targetVM.validation.zip.valid).to.be(true)
+      })
+    })
+
+    describe('valid', function () {
+      it('should be true', function () {
+        expect(targetVM.valid).to.be(true)
+      })
+    })
+  })
+})
