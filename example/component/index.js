11f6da0fdc14cdb3143948ebbb12e289d795e5a4
kazuya kawaguchi <kawakazu80@gmail.com>
2014-12-12 00:45:20 +0900

    Update component example

--- example/component/index.js
+++ example/component/index.js
@@ -4,10 +4,17 @@ var validator = require('vue-validator')
 Vue.use(validator)
 
 var form = new Vue({
-  el: '#user-form',
+  validator: {
+    validates: {
+      email: function (val) {
+        return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(val)
+      }
+    }
+  },
   data: {
     name: '',
     age: 18,
+    email: '',
     zip: '11112222'
   }
-})
+}).$mount('#user-form')
