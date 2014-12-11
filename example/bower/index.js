3934383f31efaf481418c31d2bbfcf1c49547384
kazuya kawaguchi <kawakazu80@gmail.com>
2014-12-12 00:44:04 +0900

    Update bower example

--- example/bower/index.js
+++ example/bower/index.js
@@ -3,10 +3,17 @@ var validator = window['vue-validator']
 Vue.use(validator)
 
 var form = new Vue({
-  el: '#user-form',
+  validator: {
+    validates: {
+      email: function (val) {
+        return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(val)
+      }
+    }
+  },
   data: {
     name: '',
     age: 18,
+    email: '',
     zip: '11112222'
   }
-})
+}).$mount('#user-form')
