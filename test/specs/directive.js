2041b25fdb37fe6c706ce0e93bd327bf48a2e035
kazuya kawaguchi <kawakazu80@gmail.com>
2015-07-09 02:10:33 +0900

    Add directive spec

---
+++ test/specs/directive.js
@@ -0,0 +1,73 @@
+/**
+ * Import(s)
+ */
+
+var Vue = require('../../node_modules/vue/dist/vue')
+var plugin = require('../../index')
+var createInstance = require('./helper').createInstance
+
+
+describe('directive', function () {
+  var vm
+
+  before(function () {
+    Vue.use(plugin)
+  })
+
+
+  describe('basic', function () {
+    beforeEach(function (done) {
+      vm = createInstance({
+        template: '<input type="text" v-model="msg" v-validate="required">',
+        data: { msg: '' }
+      })
+
+      Vue.nextTick(done)
+    })
+
+    it('should be validated', function () {
+      expect(vm.validation.msg.required).to.be(true)
+      expect(vm.validation.msg.valid).to.be(false)
+      expect(vm.validation.msg.invalid).to.be(true)
+      expect(vm.validation.msg.dirty).to.be(false)
+      expect(vm.valid).to.be(false)
+      expect(vm.invalid).to.be(true)
+      expect(vm.dirty).to.be(false)
+    })
+  })
+
+
+  describe('specify empty', function () {
+    beforeEach(function (done) {
+      var template = '<div v-if="enabled">'
+        + '<input type="text" v-model="foo" v-validate="">'
+        + '<input type="text" v-model="bar" v-validate="min: 0, max:5">'
+        + '</div>'
+      vm = createInstance({
+        template: template,
+        data: { foo: 'hello', bar: 1, enabled: true }
+      })
+
+      Vue.nextTick(done)
+    })
+
+    describe('foo', function () {
+      it('should be ignored', function () {
+        expect(vm.validation.foo).to.be(undefined)
+      })
+    })
+
+    describe('bar', function () {
+      it('should be validated', function () {
+        expect(vm.validation.bar.min).to.be(false)
+        expect(vm.validation.bar.max).to.be(false)
+        expect(vm.validation.bar.valid).to.be(true)
+        expect(vm.validation.bar.invalid).to.be(false)
+        expect(vm.validation.bar.dirty).to.be(false)
+        expect(vm.valid).to.be(true)
+        expect(vm.invalid).to.be(false)
+        expect(vm.dirty).to.be(false)
+      })
+    })
+  })
+})
