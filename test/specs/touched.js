881e437aaa01be2e1093d35ac35106e965ed8d17
kazuya kawaguchi <kawakazu80@gmail.com>
2015-11-20 00:26:08 +0900

    Add touched validation property tests

--- test/specs/touched.js
+++ test/specs/touched.js
@@ -6,8 +6,67 @@ import { trigger } from '../../src/util'
 describe('touched', () => {
   let el, vm
 
-  beforeEach(() => {
+  beforeEach((done) => {
     el = document.createElement('div')
+    el.innerHTML = 
+      '<validator name="validator1">' +
+      '<form novalidate>' +
+      '<input type="text" v-validate:field1.required>' +
+      '<input type="text" v-validate:field2.required>' +
+      '</form>' +
+      '</validator>'
+    vm = new Vue({
+      el: el
+    })
+    vm.$nextTick(done)
   })
 
+  context('default', () => {
+    it('field1.touched should be false', () => {
+      assert(vm.$validator1.field1.touched === false)
+    })
+    it('field2.touched should be false', () => {
+      assert(vm.$validator1.field2.touched === false)
+    })
+    it('top level touched should be false', () => {
+      assert(vm.$validator1.touched === false)
+    })
+
+    context('occured blur on field1', () => {
+      let field1, field2
+      beforeEach((done) => {
+        field1 = el.getElementsByTagName('input')[0]
+        field2 = el.getElementsByTagName('input')[1]
+        trigger(field1, 'blur')
+        vm.$nextTick(done)
+      })
+
+      it('field1.touched should be true', () => {
+        assert(vm.$validator1.field1.touched === true)
+      })
+      it('field2.touched should be false', () => {
+        assert(vm.$validator1.field2.touched === false)
+      })
+      it('top level touched should be true', () => {
+        assert(vm.$validator1.touched === true)
+      })
+
+      context('occured blur on field2', () => {
+        beforeEach((done) => {
+          trigger(field2, 'blur')
+          vm.$nextTick(done)
+        })
+
+        it('field1.touched should be true', () => {
+          assert(vm.$validator1.field1.touched === true)
+        })
+        it('field2.touched should be true', () => {
+          assert(vm.$validator1.field2.touched === true)
+        })
+        it('top level touched should be true', () => {
+          assert(vm.$validator1.touched === true)
+        })
+      })
+    })
+  })
 })
