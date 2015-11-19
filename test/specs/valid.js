940b21d3823d91453cac08b0a1d81c7bcfc451a1
kazuya kawaguchi <kawakazu80@gmail.com>
2015-11-20 01:13:10 +0900

    Add valid validation property tests

--- test/specs/valid.js
+++ test/specs/valid.js
@@ -6,26 +6,67 @@ import { trigger } from '../../src/util'
 describe('valid', () => {
   let el, vm
 
-  beforeEach(() => {
+  beforeEach((done) => {
     el = document.createElement('div')
+    el.innerHTML = 
+      '<validator name="validator1">' +
+      '<form novalidate>' +
+      '<input type="text" v-validate:field1.required.maxlength="{ maxlength: 3 }">' +
+      '<input type="text" value="foo" v-validate:field2.required.minlength="{ minlength: 1 }">' +
+      '</form>' +
+      '</validator>'
+    vm = new Vue({
+      el: el
+    })
+    vm.$nextTick(done)
   })
 
-
   context('default', () => {
-    describe('each field', () => {
-      context('field1', () => {
-        it.skip('should be false', () => {
-        })
-      })
-
-      context('field1', () => {
-        it.skip('should be false', () => {
-        })
-      })
+    it('field1.valid should be false', () => {
+      assert(vm.$validator1.field1.valid === false)
+    })
+    it('field2.valid should be true', () => {
+      assert(vm.$validator1.field2.valid === true)
+    })
+    it('top level valid should be false', () => {
+      assert(vm.$validator1.valid === false)
     })
 
-    describe('entire validator', () => {
-      it.skip('should be false', () => {
+    context('input value', () => {
+      let field1
+      beforeEach((done) => {
+        field1 = el.getElementsByTagName('input')[0]
+        field1.value = 'hi'
+        trigger(field1, 'input')
+        vm.$nextTick(done)
+      })
+
+      it('field1.valid should be true', () => {
+        assert(vm.$validator1.field1.valid === true)
+      })
+      it('field2.valid should be true', () => {
+        assert(vm.$validator1.field2.valid === true)
+      })
+      it('top level valid should be true', () => {
+        assert(vm.$validator1.valid === true)
+      })
+
+      context('back to default value', () => {
+        beforeEach((done) => {
+          field1.value = ''
+          trigger(field1, 'input')
+          vm.$nextTick(done)
+        })
+
+        it('field1.valid should be false', () => {
+          assert(vm.$validator1.field1.valid === false)
+        })
+        it('field2.valid should be true', () => {
+          assert(vm.$validator1.field2.valid === true)
+        })
+        it('top level valid should be false', () => {
+          assert(vm.$validator1.valid === false)
+        })
       })
     })
   })
