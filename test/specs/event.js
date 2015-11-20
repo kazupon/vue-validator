cc00ac757548e07fe7e3612871257c5d4e84973f
kazuya kawaguchi <kawakazu80@gmail.com>
2015-11-20 16:29:15 +0900

    Add validation event tests

--- test/specs/event.js
+++ test/specs/event.js
@@ -10,4 +10,48 @@ describe('event', () => {
     el = document.createElement('div')
   })
 
+
+  describe('valid', () => {
+    it('should be occured event', (done) => {
+      el.innerHTML = 
+        '<validator name="validator1">' +
+        '<input type="text" @valid="onValid" v-validate:field1.required>' +
+        '</validator>'
+      vm = new Vue({
+        el: el,
+        methods: {
+          onValid () {
+            done()
+          }
+        }
+      })
+      vm.$nextTick(() => {
+        let input = el.getElementsByTagName('input')[0]
+        input.value = 'foo'
+        trigger(input, 'input')
+      })
+    })
+  })
+
+  describe('invalid', () => {
+    it('should be occured event', (done) => {
+      el.innerHTML = 
+        '<validator name="validator1">' +
+        '<input type="text" value="hello" @invalid="onInValid" v-validate:field1.required>' +
+        '</validator>'
+      vm = new Vue({
+        el: el,
+        methods: {
+          onInValid () {
+            done()
+          }
+        }
+      })
+      vm.$nextTick(() => {
+        let input = el.getElementsByTagName('input')[0]
+        input.value = ''
+        trigger(input, 'input')
+      })
+    })
+  })
 })
