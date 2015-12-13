abc7f83506e37fd412e72e8b6d11694e7a4d95e7
kazuya kawaguchi <kawakazu80@gmail.com>
2015-12-14 02:09:54 +0900

    chore(build): add replacing js and README

--- config/build.js
+++ config/build.js
@@ -10,8 +10,17 @@ var banner = require('./banner')
 // update main file
 var main = fs
   .readFileSync('src/index.js', 'utf-8')
-  .replace(/plugin\.version = '[\d\.]+'/, "plugin.version = '" + pack.version + "'")
+  .replace(/plugin\.version = '[\d\.]+-alpha\.\d'/, "plugin.version = '" + pack.version + "'")
 fs.writeFileSync('src/index.js', main)
+
+// update README
+var readme = fs
+  .readFileSync('README.md', 'utf-8')
+  .replace(
+    /\<script src=\"https\:\/\/cdn\.jsdelivr\.net\/vue\.validator\/[\d\.]+-alpha\.\d\/vue-validator\.min\.js\"\>\<\/script\>/,
+    '<script src="https://cdn.jsdelivr.net/vue.validator/' + pack.version + '/vue-validator.min.js"></script>'
+  )
+fs.writeFileSync('README.md', readme)
 
 // CommonJS build.
 // this is used as the "main" field in package.json
