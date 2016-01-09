f81276f179a522203448750e34fbc8bb44be0014
kazuya kawaguchi <kawakazu80@gmail.com>
2016-01-10 03:07:31 +0900

    test(validator): add v-for cases

--- test/specs/directives/validator.js
+++ test/specs/directives/validator.js
@@ -1,5 +1,6 @@
 import assert from 'power-assert'
 import Vue from 'vue'
+import { trigger } from '../../../src/util'
 
 
 describe('validator element directive', () => {
@@ -248,6 +249,88 @@ describe('validator element directive', () => {
             assert(vm.$validator1.pristine !== undefined)
           })
         })
+      })
+    })
+  })
+
+
+  describe('v-for', () => {
+    beforeEach((done) => {
+      el.innerHTML = '<ul><li v-for="task in tasks">' + 
+        '<validator :name="\'validator\' + $index">' +
+        '<form novalidate>' + 
+        '<input type="text" :value="task.name" v-validate:name="{ required: true, minlength: calculate($index) }">' +
+        '</form>' +
+        '</validator>' +
+        '<pre>{{$validator0|json}}</pre>' +
+        '<pre>{{$validator1|json}}</pre>' +
+        '<pre>{{$validator2|json}}</pre>' +
+        '</li></ul>' + 
+        '<button @click="onAdd">add</button>'
+      vm = new Vue({
+        el: el,
+        data: {
+          counter: 2,
+          tasks: [{
+            name: 'task1', priority: 1
+          }, {
+            name: 'task2', priority: 2
+          }]
+        },
+        methods: {
+          getValidator (index) {
+            return 
+          },
+          calculate (index) {
+            return (index + 1) * 4
+          },
+          add (name, priority) {
+            this.tasks.push({ name: name, priority })
+          },
+          onAdd () {
+            let priority = this.counter++
+            this.add('task' + priority, priority)
+          }
+        }
+      })
+      vm.$nextTick(done)
+    })
+
+    context('initial render', () => {
+      it('should be assigned', (done) => {
+        // default
+        assert(vm.$validator0.name.required === false)
+        assert(vm.$validator0.name.minlength === false)
+        assert(vm.$validator1.name.required === false)
+        assert(vm.$validator1.name.minlength === true)
+
+        let name1 = el.getElementsByTagName('input')[0]
+        let name2 = el.getElementsByTagName('input')[1]
+        name1.value = ''
+        name2.value = 'tasktask2'
+        trigger(name1, 'input')
+        trigger(name2, 'input')
+        vm.$nextTick(() => {
+          assert(vm.$validator0.name.required === true)
+          assert(vm.$validator0.name.minlength === true)
+          assert(vm.$validator1.name.required === false)
+          assert(vm.$validator1.name.minlength === false)
+          done()
+        })
+      })
+    })
+
+    context('dynamic render', () => {
+      beforeEach((done) => {
+        let button = el.getElementsByTagName('button')[0]
+        trigger(button, 'click')
+        vm.$nextTick(done)
+      })
+
+      it('should be assigned', (done) => {
+        assert(vm.$validator2.name.required === false)
+        assert(vm.$validator2.name.minlength === true)
+        done()
       })
     })
   })
