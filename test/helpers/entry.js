24cd05f800c88133b65f409fa6c0c333218ca665
kazuya kawaguchi <kawakazu80@gmail.com>
2016-06-14 22:59:19 +0900

    :up: update(unit): entrypoint helper

--- test/helpers/entry.js
+++ test/helpers/entry.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+import { compileToFunctions } from 'vue-template-compiler'
 import assert from 'power-assert'
 import plugin from '../../src/index'
 
 Vue.use(plugin)
 
 window.Vue = Vue
+window.compileToFunctions = compileToFunctions
 window.assert = assert
