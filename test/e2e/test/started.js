4fc7bd64666efe92b07d824e15602c35c2ddc659
kazuya kawaguchi <kawakazu80@gmail.com>
2016-07-19 00:43:10 +0900

    :green_heart: test(ci): fix eslint errors

--- test/e2e/test/started.js
+++ test/e2e/test/started.js
@@ -2,8 +2,10 @@ module.exports = {
   add: function (browser) {
     browser
       .url('http://localhost:8080/examples/started/')
+      /*
+      .waitForElementVisible('p', 1000)
+      .assert.containsText('p', '3', 'You should be implemented !!')
+      */
       .end()
-      //.waitForElementVisible('p', 1000)
-      //.assert.containsText('p', '3', 'You should be implemented !!')
   }
 }
