a8f762a236a67df90eb03daf4329be0218419147
kazuya kawaguchi <kawakazu80@gmail.com>
2016-07-27 16:29:26 +0900

    :up: test(helper): import polyfill

--- test/helpers/entry.js
+++ test/helpers/entry.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import { compileToFunctions } from 'vue-template-compiler'
 import assert from 'power-assert'
 import plugin from '../../src/index'
+import 'babel-polyfill' // promise and etc ...
 
 Vue.use(plugin)
 
