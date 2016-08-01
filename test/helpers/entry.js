5d74bae9e700d643707b72d4c897361003c7f560
kazuya kawaguchi <kawakazu80@gmail.com>
2016-08-01 18:56:12 +0900

    :shirt: refactor: unneccesary codes

--- test/helpers/entry.js
+++ test/helpers/entry.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import { compileToFunctions } from 'vue-template-compiler'
 import assert from 'power-assert'
 import plugin from '../../src/index'
 import 'babel-polyfill' // promise and etc ...
@@ -7,5 +6,4 @@ import 'babel-polyfill' // promise and etc ...
 Vue.use(plugin)
 
 window.Vue = Vue
-window.compileToFunctions = compileToFunctions
 window.assert = assert
