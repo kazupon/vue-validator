267dcc9cd54664a4004e2ceb543425e964444de2
kazuya kawaguchi <kawakazu80@gmail.com>
2016-09-29 10:17:37 +0900

    :up: update: add validation class [ci skip]

---
+++ src/validation.js
@@ -0,0 +1,198 @@
+/* @flow */
+import Group from './group'
+
+export default function (Vue: any) {
+  const ValidityGroup = Group(Vue)
+
+  class Validation {
+    // TODO: should be defined type
+    _host: any
+    _validities: Object
+    _host: any
+    _result: Object
+    _named: Object
+    _group: Object
+    _watcher: Function
+    _validityManager: any
+    _beginDestroy: boolean
+
+    constructor (options: Object = {}) {
+      this._result = {}
+      this._host = options.host
+      this._named = Object.create(null)
+      this._group = Object.create(null)
+      this._validities = Object.create(null)
+      this._beginDestroy = false
+      Vue.util.defineReactive(this._host, '$validation', this._result)
+    }
+
+    register (
+      field: string,
+      validity: ValidityComponent,
+      options: { named?: string, group?: string } = {}
+    ): void {
+      // NOTE: lazy setup (in constructor, occured callstack recursive errors ...)
+      if (!this._validityManager) {
+        this._validityManager = new Vue(ValidityGroup)
+        this._watchValidityResult()
+      }
+
+      if (this._validities[field]) {
+        // TODO: should be output console.error
+        return
+      }
+      this._validities[field] = validity
+
+      const { named, group } = options
+      const groupValidity = group 
+        ? this._getValidityGroup('group', group) || this._registerValidityGroup('group', group)
+        : null
+      const namedValidity = named
+        ? this._getValidityGroup('named', named) || this._registerValidityGroup('named',named)
+        : null
+      if (namedValidity && groupValidity) {
+        groupValidity.register(field, validity)
+        !namedValidity.isRegistered(group) && namedValidity.register(group, groupValidity)
+        !this._validityManager.isRegistered(named) && this._validityManager.register(named, namedValidity)
+      } else if (namedValidity) {
+        namedValidity.register(field, validity)
+        !this._validityManager.isRegistered(named) && this._validityManager.register(named, namedValidity)
+      } else if (groupValidity) {
+        groupValidity.register(field, validity)
+        !this._validityManager.isRegistered(group) && this._validityManager.register(group, groupValidity)
+      } else {
+        this._validityManager.register(field, validity)
+      }
+    }
+
+    unregister (
+      field: string,
+      options: { named?: string, group?: string } = {}
+    ): void {
+      if (!this._validityManager) {
+        // TODO: should be output error
+        return
+      }
+
+      if (!this._validities[field]) {
+        // TODO: should be output error
+        return
+      }
+      delete this._validities[field]
+
+      const { named, group } = options
+      const groupValidity = group ? this._getValidityGroup('group', group) : null
+      const namedValidity = named ? this._getValidityGroup('named', named) : null
+      if (namedValidity && groupValidity) {
+        groupValidity.unregister(field)
+        namedValidity.isRegistered(group) && namedValidity.unregister(group)
+        this._validityManager.isRegistered(named) && this._validityManager.unregister(named)
+      } else if (namedValidity) {
+        namedValidity.unregister(field)
+        this._validityManager.isRegistered(named) && this._validityManager.unregister(named)
+      } else if (groupValidity) {
+        groupValidity.unregister(field)
+        this._validityManager.isRegistered(group) && this._validityManager.unregister(group)
+      } else {
+        this._validityManager.unregister(field)
+      }
+
+      group && this._unregisterValidityGroup('group', group)
+      named && this._unregisterValidityGroup('named', named)
+    }
+
+    destroy (): void {
+      const validityKeys = Object.keys(this._validities)
+      const namedKeys = Object.keys(this._named)
+      const groupKeys = Object.keys(this._group)
+
+      // unregister validity
+      validityKeys.forEach((validityKey: string) => {
+        groupKeys.forEach((groupKey: string) => {
+          const group = this._getValidityGroup('group', groupKey)
+          if (group && group.isRegistered(validityKeys)) {
+            group.unregister(validityKey)
+          }
+        })
+        namedKeys.forEach((namedKey: string) => {
+          const named = this._getValidityGroup('named', namedKey)
+          if (named && named.isRegistered(validityKey)) {
+            named.unregister(validityKey)
+          }
+        })
+        if (this._validityManager.isRegistered(validityKey)) {
+          this._validityManager.unregister(validityKey)
+        }
+        delete this._validities[validityKey]
+      })
+
+      // unregister grouped validity
+      groupKeys.forEach((groupKey: string) => {
+        namedKeys.forEach((namedKey: string) => {
+          const named = this._getValidityGroup('named', namedKey)
+          if (named && named.isRegistered(groupKey)) {
+            named.unregister(groupKey)
+          }
+        })
+        if (this._validityManager.isRegistered(groupKey)) {
+          this._validityManager.unregister(groupKey)
+        }
+        this._unregisterValidityGroup('group', groupKey)
+      })
+
+      // unregister named validity
+      namedKeys.forEach((namedKey: string) => {
+        if (this._validityManager.isRegistered(namedKey)) {
+          this._validityManager.unregister(namedKey)
+        }
+        this._unregisterValidityGroup('named', namedKey)
+      })
+
+      this._beginDestroy = true
+    }
+
+    _getValidityGroup (type: string, name: string): ?ValidityGroupComponent {
+      return type === 'named' ? this._named[name] : this._group[name]
+    }
+
+    _registerValidityGroup (type: string, name: string): ValidityGroupComponent {
+      const groups = type === 'named' ? this._named : this._group
+      groups[name] = new Vue(ValidityGroup)
+      return groups[name]
+    }
+
+    _unregisterValidityGroup (type: string, name: string): void {
+      const groups = type === 'named' ? this._named : this._group
+      if (!groups[name]) {
+        // TODO: should be warn
+        return
+      }
+
+      groups[name].$destroy()
+      delete groups[name]
+    }
+
+    _watchValidityResult (): void {
+      const validation = this
+      this._watcher = this._validityManager.$watch('results', (val: any, old: any) => {
+        Vue.set(this._host, '$validation', val)
+        if (this._beginDestroy) {
+          this._destroyValidityMananger()
+        }
+      }, { deep: true })
+    }
+
+    _unwatchValidityResult (): void {
+      this._watcher()
+      delete this._watcher
+    }
+
+    _destroyValidityMananger (): void {
+      this._unwatchValidityResult()
+      this._validityManager.$destroy()
+      this._validityManager = null
+    }
+  }
+
+  return Validation
+}
---
+++ test/unit/validation.test.js
@@ -0,0 +1,108 @@
+import Validity from '../../src/components/validity/index'
+import ValidationClass from '../../src/validation'
+
+const { extend } = Vue.util
+const validity = Validity(Vue)
+const Validation = ValidationClass(Vue)
+
+describe('validation class', () => {
+  let validation, vm
+  function createValidity (field) {
+    const options = {}
+    extend(options, validity)
+    options.propsData = {
+      field,
+      child: {}, // dummy
+      validators: {
+        required: true
+      }
+    }
+    return new Vue(options)
+  }
+
+  beforeEach(() => {
+    vm = new Vue()
+    validation = new Validation({ host: vm })
+  })
+
+  describe('register/unregsiter', () => {
+    it('should be work', done => {
+      let validation1, group1, group2, field4
+      // add validation1 and field1
+      validation.register('field1', createValidity('field1'), { named: 'validation1' })
+      waitForUpdate(() => {
+        assert('validation1' in vm.$validation)
+        validation1 = vm.$validation.validation1
+        assert('valid' in validation1)
+        assert('invalid' in validation1)
+        assert('touched' in validation1)
+        assert('untouched' in validation1)
+        assert('dirty' in validation1)
+        assert('pristine' in validation1)
+        assert('modified' in validation1)
+        assert('field1' in validation1)
+        // add field2 only
+        validation.register('field2', createValidity('field2'))
+      }).then(() => {
+        assert('field2' in vm.$validation)
+        // add validation2, group1 and field3
+        validation.register('field3', createValidity('field3'), { named: 'validation2', group: 'group1' })
+      }).then(() => {
+        assert('validation2' in vm.$validation)
+        assert('group1' in vm.$validation.validation2)
+        group1 = vm.$validation.validation2.group1
+        assert('valid' in group1)
+        assert('invalid' in group1)
+        assert('touched' in group1)
+        assert('untouched' in group1)
+        assert('dirty' in group1)
+        assert('pristine' in group1)
+        assert('modified' in group1)
+        assert('field3' in group1)
+        // add group2 and field4
+        validation.register('field4', createValidity('field4'), { group: 'group2' })
+      }).then(() => {
+        assert('group2' in vm.$validation)
+        group2 = vm.$validation.group2
+        assert('valid' in group2)
+        assert('invalid' in group2)
+        assert('touched' in group2)
+        assert('untouched' in group2)
+        assert('dirty' in group2)
+        assert('pristine' in group2)
+        assert('modified' in group2)
+        assert('field4' in group2)
+        // remove group2 and field4
+        validation.unregister('field4', { group: 'group2' })
+      }).then(() => {
+        assert(!('group2' in vm.$validation))
+        // remove validation2, group1 and field3
+        validation.unregister('field3', { named: 'validation2', group: 'group1' })
+      }).then(() => {
+        assert(!('validation2' in vm.$validation))
+        // remove field2 only
+        validation.unregister('field2')
+      }).then(() => {
+        assert(!('field2' in vm.$validation))
+        // remove validation1 and field1
+        validation.unregister('field1', { named: 'validation1' })
+      }).then(() => {
+        assert(!('validation1' in vm.$validation))
+      }).then(done)
+    })
+  })
+
+  describe('destroy', () => {
+    it('should be work', done => {
+      validation.register('field1', createValidity('field1'), { named: 'validation1' })
+      validation.register('field2', createValidity('field2'))
+      validation.register('field3', createValidity('field3'), { named: 'validation2', group: 'group1' })
+      validation.register('field4', createValidity('field4'), { group: 'group2' })
+      waitForUpdate(() => {
+        validation.destroy()
+      }).then(() => {
+        assert.equal(JSON.stringify(vm.$validation), JSON.stringify({}))
+      }).then(done)
+    })
+  })
+})
