9fd4af474f43c786dacb5f58ad844da310427f2c
kazuya kawaguchi <kawakazu80@gmail.com>
2016-10-27 11:13:51 +0900

    :green_heart: test(started): add getting started example tests

--- test/e2e/test/started.js
+++ test/e2e/test/started.js
@@ -1,11 +1,26 @@
 module.exports = {
-  add: function (browser) {
+  started: function (browser) {
     browser
       .url('http://localhost:8080/examples/started/')
-      /*
-      .waitForElementVisible('p', 1000)
-      .assert.containsText('p', '3', 'You should be implemented !!')
-      */
+      // initial loaded
+      .waitForElementVisible('#app', 1000)
+      .waitForElementNotPresent('.errors .required', 1000)
+      .waitForElementNotPresent('.errors .minlength', 1000)
+      // minlength invalid
+      .setValue('input', 'foo')
+      .trigger('input', 'input')
+      .waitForElementNotPresent('.errors .required', 1000)
+      .waitForElementPresent('.errors .minlength', 1000)
+      // and required invalid
+      .clearValue('input')
+      .trigger('input', 'input')
+      .waitForElementPresent('.errors .required', 1000)
+      .waitForElementPresent('.errors .minlength', 1000)
+      // valid !!
+      .setValue('input', 'kazupon')
+      .trigger('input', 'input')
+      .waitForElementNotPresent('.errors .required', 1000)
+      .waitForElementNotPresent('.errors .minlength', 1000)
       .end()
   }
 }
