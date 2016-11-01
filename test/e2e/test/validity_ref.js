8a199fe82d94ec4fa6dc9737327107a775dc60e4
kazuya kawaguchi <kawakazu80@gmail.com>
2016-11-01 10:53:52 +0900

    :white_check_mark: test(e2e): add test cases

---
+++ test/e2e/test/validity_ref.js
@@ -0,0 +1,26 @@
+module.exports = {
+  started: function (browser) {
+    browser
+      .url('http://localhost:8080/examples/validity/ref/')
+      // initial loaded
+      .waitForElementVisible('#app', 1000)
+      .waitForElementNotPresent('.errors .required', 1000)
+      .waitForElementNotPresent('.errors .minlength', 1000)
+      // minlength invalid
+      .setValue('input', 'foo')
+      .trigger('input', 'input')
+      .waitForElementNotPresent('.errors .required', 1000)
+      .waitForElementPresent('.errors .minlength', 1000)
+      // and required invalid
+      .clearValue('input')
+      .trigger('input', 'input')
+      .waitForElementPresent('.errors .required', 1000)
+      .waitForElementPresent('.errors .minlength', 1000)
+      // valid !!
+      .setValue('input', 'kazupon')
+      .trigger('input', 'input')
+      .waitForElementNotPresent('.errors .required', 1000)
+      .waitForElementNotPresent('.errors .minlength', 1000)
+      .end()
+  }
+}
